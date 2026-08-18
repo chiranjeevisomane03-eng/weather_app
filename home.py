c4b73441dcafb6874b23320923cc26a57c10422f
Chiranjeevi2008 <chiranjeevisomane03@gmail.com>
2026-08-19 01:18:47 +0530

    Update home.py

--- home.py
+++ home.py
@@ -12,7 +12,7 @@ address=location.json()
 if(address["status"]=="success"):
     with st.skeleton():
         time.sleep(1)
-    st.success("✅"+"Location Detected!",icon="spinner",title="Live")
+    st.success("✅"+"Connection established!",icon="spinner",title="Live")
 request=requests.get(f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={address["city"]}&aqi=yes")
 request=request.json()
 a,c,d=st.columns([1.9,1.5,1.5])
